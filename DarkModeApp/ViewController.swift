8a43e17f7368146b397db9a46f80added82539b5
Bera <burakekici44@gmail.com>
2019-11-22 10:18:00 +0300

    Kullanıcın telefonunun hangi modda olduğunu çektiğimiz uygulama ve bu çektiğimiz veriye göre ayarlamalar

--- DarkModeApp/ViewController.swift
+++ DarkModeApp/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Uygulamanın hangi modda kalmasını istiyorsak onu belirtiyoruz.
+        overrideUserInterfaceStyle = .light
     }
-
-
+    //Başka bir uygulamaya gidildiğinde yani telefon ile ilgili ayarlar değiştirilip geri uygulamaya dönülürse değişikliklerin anlık olarak uygulanabilmesi için bu fonkisyon çağırılır.
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        
+        //traitCollection ile kullanıcının telefon özelliklerine ulaşabiliyoruz.
+        //Kullanıcının telefonu hangi modda kullanıldığını görmek için userInterfaceStyle değerini çağırıyoruz.
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        if userInterfaceStyle == .dark{
+            changeButton.tintColor = UIColor.white
+        }
+        else{
+            changeButton.tintColor = UIColor.blue
+        }
+    }
 }
 
